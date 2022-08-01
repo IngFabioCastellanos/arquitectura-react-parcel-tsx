bc32066301c6fb65b9240cea9a3e2e98d818026b
ESAGOBOLIVAR <36249220+IngFabioCastellanos@users.noreply.github.com>
2022-07-31 20:17:12 -0500

    ajuste en las acciones para llamar la api

--- frontend/src/api/apiActions.ts
+++ frontend/src/api/apiActions.ts
@@ -1,9 +1,18 @@
 import { api } from "./api";
 
 export const apiActions = {
-  findAllByNamesheet: async (props: findAllByNamesheet) => await api(props),
-  findDataById: async (props: findAllByNamesheet) => await api(props),
-  findMultipleDataById: async (props: findMultipleDataById) => await api(props),
-  saveObjectInSheet: async (props: saveObjectInSheet) => await api(props),
-  updateObjectInSheet: async (props: updateObjectInSheet) => await api(props),
+  findAllByNamesheet: async (props: findAllByNamesheet) =>
+    await api({ action: "findAllByNamesheet", ...props }),
+
+  findDataById: async (props: findAllByNamesheet) =>
+    await api({ action: "findDataById", ...props }),
+
+  findMultipleDataById: async (props: findMultipleDataById) =>
+    await api({ action: "findMultipleDataById", ...props }),
+
+  saveObjectInSheet: async (props: saveObjectInSheet) =>
+    await api({ action: "saveObjectInSheet", ...props }),
+
+  updateObjectInSheet: async (props: updateObjectInSheet) =>
+    await api({ action: "updateObjectInSheet", ...props }),
 }
--- frontend/src/views/Prueba.tsx
+++ frontend/src/views/Prueba.tsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { apiActions } from '../api/apiActions'
+import { nameSheets } from '../api/nameSheets'
 
 export default function Prueba() {
+
+  useEffect(() => {
+
+    (async () => {
+      console.log(
+        await apiActions.findAllByNamesheet({
+          nameSheet: nameSheets.USUARIOS
+        })
+      );
+    })()
+  }, [])
+
+
   return (
     <div>Prueba</div>
   )
