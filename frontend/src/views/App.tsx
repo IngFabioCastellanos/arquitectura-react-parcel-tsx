c6bfc11e1b9e4f8944dc1ddcffd7efd30b43fd5d
fabio ivan castellanos silva <36249220+IngFabioCastellanos@users.noreply.github.com>
2022-11-02 18:05:26 -0500

    Update App.tsx

--- frontend/src/views/App.tsx
+++ frontend/src/views/App.tsx
@@ -1,35 +1,11 @@
 import React from 'react'
-import { useListasDesplegables } from '../store/store';
 import { Button } from 'antd';
 import { Link } from 'react-router-dom';
 
 export default function App() {
 
-  const listasDesplegables = useListasDesplegables();
-
-  listasDesplegables.get().ciudades.map(e => console.log(e));
-
-  // try {
-  //   google.script.run
-  //     .withSuccessHandler((e: Array<any>) => console.log(e))
-  //     .obtenerDatos("registro usuarios")
-
-  // } catch (error) {
-  //   console.log(error);
-
-  // }
-
-
-
   return (
     <>
-      {
-        listasDesplegables.get().ciudades.map((e, index) =>
-          <button
-            key={index}
-            className='font-bold text-2xl border-2 rounded-full bg-yellow-300 text-green-800 py-2 px-10 hover:shadow-lg hover:shadow-green-500 duration-700'>{e}</button>
-        )
-      }
       <Link to={"/prueba"}>
         <Button type="primary">Ruta prueba</Button>
       </Link>
