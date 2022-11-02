810a2eb9ad5c5ee186f003c0b066ce3c86b1aa65
fabio ivan castellanos silva <36249220+IngFabioCastellanos@users.noreply.github.com>
2022-11-02 18:04:30 -0500

    Update interfaceApi.ts

--- frontend/src/interfaces/api/interfaceApi.ts
+++ frontend/src/interfaces/api/interfaceApi.ts
@@ -1,9 +1,27 @@
-interface findAllByNamesheet { nameSheet: string; }
+export interface findAllByNamesheet {
+  nameSheet: string;
+}
 
-interface findDataById { nameSheet: string, id: string, nameId: string }
+export interface findDataById {
+  nameSheet: string;
+  id: string;
+  nameId: string;
+}
 
-interface findMultipleDataById { nameSheet: string, id: string, nameId: string }
+export interface findMultipleDataById {
+  nameSheet: string;
+  id: string;
+  nameId: string;
+}
 
-interface saveObjectInSheet { data: string, nameSheet: string }
+export interface saveObjectInSheet {
+  data: string;
+  nameSheet: string;
+}
 
-interface updateObjectInSheet { id: string, datos: string, nameSheet: string, nameId: string }
+export interface updateObjectInSheet {
+  id: string;
+  datos: string;
+  nameSheet: string;
+  nameId: string;
+}
