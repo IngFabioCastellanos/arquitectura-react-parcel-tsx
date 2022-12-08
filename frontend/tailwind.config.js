847ad368d3db51e25853bb0ea970fd7b7fa40c31
ESAGOBOLIVAR <105010505+ESAGOBOLIVAR@users.noreply.github.com>
2022-12-08 10:08:22 -0500

    update antd v5 y tailwind css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+  corePlugins: {
+    preflight: false, // <== disable this!
+  },
+};
